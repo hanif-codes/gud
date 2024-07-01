01fc0b92daa4af414262cc192aba7a5a257e5c4e
hanif <thisisanemail@email.com>
2024-07-01 19:05:00 +0100

    init now creates a folder

--- src/commands.py
+++ src/commands.py
@@ -1,9 +1,17 @@
 """
 All of these are commands that will ultimately be used as `gud <command_name>`
 """
+import os
+import sys
+
+executed_from = os.getcwd()
 
 def hello():
     print("Hello there.")
 
 def init():
-    print("Initialising repository...")
+    try:
+        os.makedirs(".gud", exist_ok=False)
+    except FileExistsError:
+        sys.exit(f"Repository {executed_from}/.gud already exists")
+    print(f"Initialising Gud repository in {executed_from}/.gud")
