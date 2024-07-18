3338066c71fff0ac43f789f0f49598c1eed86cba
hanif <thisisanemail@email.com>
2024-07-18 15:47:13 +0300

    view repo config file works. need to figure out editing and where to store the default config file

--- gud/commands.py
+++ gud/commands.py
@@ -61,29 +61,43 @@ def init(invocation):
 
 def config(invocation):
 
+    # determine which modes the user wants
     if not invocation.args["view"] and not invocation.args["edit"]:
-
         # if the --default flag is passed, but neither --view nor --edit is specified, this is invalid usage
         if invocation.args["default"]:
             sys.exit("Since you provided a --default flag, you must also provide either a --view or --edit flag.")
-
         # otherwise, this means no flags were provided
-        # therefore, take them through a questionary prompt
-        print("You provided no flags!")
-
+        view_or_edit = questionary.select(
+            "Would you like to view or edit a config file?",
+            ["View", "Edit"]
+        ).ask().lower()
+        repo_or_default = questionary.select(
+            f"Would you like to {view_or_edit} this repository's configuration settings, or the global default configuration settings?",
+            ["Repository", "Default"]
+        ).ask().lower()
     else:
-
-        if invocation.args["default"]:
-            config_path = invocation.repo.config_path # TODO - change this to wherever the defaults will get stored
-            heading_string = "Default config options:"
+        if invocation.args["edit"]:
+            view_or_edit = "edit"
         else:
-            config_path = invocation.repo.config_path
-            heading_string = f"Repository config options ({config_path}):"
+            view_or_edit = "view"
+        if invocation.args["default"]:
+            repo_or_default = "default"
+        else:
+            repo_or_default = "repository"
 
-        if invocation.args["view"]:
-            print(heading_string, "\n")
-            with open(config_path, "r", encoding="utf-8") as f:
-                print(f.read())
-        elif invocation.args["edit"]:
-            # TODO - do something relating to editing the config file
-            ...
+    # execute the command based on the modes determined above
+    if repo_or_default == "default":
+        config_path = invocation.repo.config_path # TODO - change this to wherever the defaults will get stored
+        heading_string = "Default config options:"
+    else: # else "repository"
+        config_path = invocation.repo.config_path
+        heading_string = f"Repository config options ({config_path}):"
+
+    if view_or_edit == "edit":
+        # TODO - do something relating to editing the config file
+        # On Windows, I may need to figure out how to open notepad
+        ...
+    else:
+        print(heading_string, "\n")
+        with open(config_path, "r", encoding="utf-8") as f:
+            print(f.read())
