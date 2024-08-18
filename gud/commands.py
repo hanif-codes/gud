26cccd259c487c011ea3949e56de05d6113fb905
hanif <thisisanemail@email.com>
2024-08-18 13:15:24 +0100

    pager working on linux, still need to figure out how best to make 'less' work in windows

--- gud/commands.py
+++ gud/commands.py
@@ -11,7 +11,8 @@ from .helpers import (
     get_all_ignored_paths,
     format_path_for_gudignore,
     get_file_mode,
-    open_relevant_pager
+    open_relevant_pager,
+    see_if_command_exists
 )
 from .classes import (
     Blob,
@@ -457,4 +458,10 @@ def status(invocation, print_output=True) -> dict:
 
 def log(invocation):
     op_sys = invocation.os
+    less_exists = see_if_command_exists(op_sys, "less")
+    if less_exists:
+        print(f"Gud logs are about to open...\nTo scroll, use the arrow keys. To exit, press q.")
+    else:
+        print(f"Gud logs are about to open...\nTo scroll down, use the spacebar. To exit, press q.")
+    questionary.press_any_key_to_continue().ask()
     open_relevant_pager(op_sys, "notes.txt")
--- gud/helpers.py
+++ gud/helpers.py
@@ -47,11 +47,27 @@ def open_relevant_editor(op_sys: OperatingSystem, file_path: str) -> None:
             subprocess.call(["nano", file_path])
 
 
-def open_relevant_pager(op_sys: OperatingSystem, file_path: str) -> None:
-    """ TODO - implement this """    
+def see_if_command_exists(op_sys: OperatingSystem, command: str):
+        try:
+            match op_sys.name:
+                case "WINDOWS":
+                    os.system(f"{command} --version")
+                case "MAC_OS":
+                    subprocess.call(f"{command} --version")
+                case "LINUX":
+                    subprocess.call(f"{command} --version")
+        except FileNotFoundError:
+            return False
+        return True
+
+
+def open_relevant_pager(op_sys: OperatingSystem, file_path: str) -> None:    
     match op_sys.name:
         case "WINDOWS":
-            os.system(f"less {file_path}")
+            try:
+                os.system(f"less {file_path}")
+            except FileNotFoundError:
+                os.system(f"more {file_path}")
         case "MAC_OS":
             subprocess.call(["less", file_path])
         case "LINUX":
